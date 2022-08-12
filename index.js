c40ecc22c0723f6498cf3094ca9aaf2cfa7a48cc
Dan Gleason <dannycgleason@gmail.com>
2022-08-12 14:39:00 -0400

    added command to bundle slack app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { App } = require("@slack/bolt");
 require('dotenv').config({ path: require('find-config')('.env') })
+const axios = require('axios')
 
 const app = new App({
     token: process.env.token, //Find in the Oauth  & Permissions tab
@@ -7,6 +8,23 @@ const app = new App({
     socketMode:true,
     appToken: process.env.SLACK_APP_TOKEN // Token from the App-level Token that we created
 });
+
+app.command("/send", async ({ command, ack, say }) => {
+    try {
+      await ack();
+      let txt = command.text // The inputted parameters
+      if(isNaN(txt)) {
+          say(txt + " is not a number")
+      } else {
+          say(txt + " squared = " + (parseFloat(txt) * parseFloat(txt)))
+      }
+    } catch (error) {
+      console.log("err")
+      console.error(error);
+    }
+});
+
+
 
 
 app.start(3000)
