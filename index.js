67f23d51be25dbb18fcf82830f2fcac078da001b
Dan Gleason <dannycgleason@gmail.com>
2022-08-12 14:28:07 -0400

    added env varialbes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { App } = require("@slack/bolt");
-
+require('dotenv').config({ path: require('find-config')('.env') })
 
 const app = new App({
-    token: "INSERT YOUR BOT USER OAUTH TOKEN", //Find in the Oauth  & Permissions tab
-    signingSecret: "INSERT SIGNING SECRET", // Find in Basic Information Tab
+    token: process.env.token, //Find in the Oauth  & Permissions tab
+    signingSecret: process.env.singingSecret, // Find in Basic Information Tab
     socketMode:true,
-    appToken: "INSERT SOCKET TOKEN" // Token from the App-level Token that we created
+    appToken: process.env.appToken // Token from the App-level Token that we created
 });
 
 
