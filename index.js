d00841251f5336a5ac674a169bd77330bc4dc001
Dan Gleason <dannycgleason@gmail.com>
2022-08-12 19:19:32 -0400

    added code

--- index.js
+++ index.js
@@ -9,10 +9,59 @@ const app = new App({
     appToken: process.env.SLACK_APP_TOKEN // Token from the App-level Token that we created
 });
 
+
+app.shortcut('send', async ({ shortcut, ack, client, logger }) => {
+
+    try {
+      // Acknowledge shortcut request
+      await ack();
+  
+      // Call the views.open method using one of the built-in WebClients
+      const result = await client.views.open({
+        trigger_id: shortcut.trigger_id,
+        view: {
+          type: "modal",
+          title: {
+            type: "plain_text",
+            text: "My App"
+          },
+          close: {
+            type: "plain_text",
+            text: "Close"
+          },
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+              }
+            },
+            {
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+                }
+              ]
+            }
+          ]
+        }
+      });
+  
+      logger.info(result);
+    }
+    catch (error) {
+      logger.error(error);
+    }
+  });
+
+
 app.command("/send", async ({ command, ack, say }) => {
     try {
         await ack();
-        
+
         const { values } = view.state;
         const taskName = values.task_name_input.name;
         const taskDescription = values.task_description_input.description;
@@ -25,6 +74,7 @@ app.command("/send", async ({ command, ack, say }) => {
             "blocks": [
                 {
                     "type": "input",
+                    "block_id": 'address_input',
                     "element": {
                         "type": "plain_text_input",
                         "action_id": "plain_text_input-action"
@@ -37,6 +87,7 @@ app.command("/send", async ({ command, ack, say }) => {
                 },
                 {
                     "type": "input",
+                    "block_id": 'city_input',
                     "element": {
                         "type": "plain_text_input",
                         "action_id": "plain_text_input-action"
@@ -49,6 +100,7 @@ app.command("/send", async ({ command, ack, say }) => {
                 },
                 {
                     "type": "input",
+                    "block_id": 'state_input',
                     "element": {
                         "type": "plain_text_input",
                         "action_id": "plain_text_input-action"
@@ -61,6 +113,7 @@ app.command("/send", async ({ command, ack, say }) => {
                 },
                 {
                     "type": "input",
+                    "block_id": 'postal_input',
                     "element": {
                         "type": "plain_text_input",
                         "action_id": "plain_text_input-action"
@@ -73,6 +126,7 @@ app.command("/send", async ({ command, ack, say }) => {
                 },
                 {
                     "type": "input",
+                    "block_id": 'country_input',
                     "element": {
                         "type": "plain_text_input",
                         "action_id": "plain_text_input-action"
@@ -85,6 +139,7 @@ app.command("/send", async ({ command, ack, say }) => {
                 },
                 {
                     "type": "input",
+                    "block_id": 'message_input',
                     "element": {
                         "type": "plain_text_input",
                         "multiline": true,
@@ -152,6 +207,9 @@ app.command("/send", async ({ command, ack, say }) => {
       console.error(error);
     }
 });
+
+
+
 // The echo command simply echoes on command
 app.command('/echo', async ({ command, ack, respond }) => {
     // Acknowledge command request
