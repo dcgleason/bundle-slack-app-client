b66b1727ac6cc4db821ddbf6a72b1d674af96b1d
Dan Gleason <dannycgleason@gmail.com>
2022-08-12 20:06:14 -0400

    changed send command

--- index.js
+++ index.js
@@ -10,66 +10,10 @@ const app = new App({
 });
 
 
-app.shortcut('send_letter', async ({ shortcut, ack, client, logger }) => {
-
-    try {
-      // Acknowledge shortcut request
-      await ack();
-  
-      // Call the views.open method using one of the built-in WebClients
-      const result = await client.views.open({
-        trigger_id: shortcut.trigger_id,
-        view: {
-          type: "modal",
-          title: {
-            type: "plain_text",
-            text: "My App"
-          },
-          close: {
-            type: "plain_text",
-            text: "Close"
-          },
-          blocks: [
-            {
-              type: "section",
-              text: {
-                type: "mrkdwn",
-                text: "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
-              }
-            },
-            {
-              type: "context",
-              elements: [
-                {
-                  type: "mrkdwn",
-                  text: "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
-                }
-              ]
-            }
-          ]
-        }
-      });
-  
-      logger.info(result);
-    }
-    catch (error) {
-      logger.error(error);
-    }
-  });
-
 
 app.command("/send", async ({ command, ack, say }) => {
     try {
         await ack();
-
-        const { values } = view.state;
-        const taskName = values.task_name_input.name;
-        const taskDescription = values.task_description_input.description;
-                    
-        const inputs = {
-          taskName: { value: taskName.value },
-          taskDescription: { value: taskDescription.value }
-        };
         await say({
             "blocks": [
                 {
