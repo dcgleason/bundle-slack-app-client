edef439dfbd4a64a982f300f6a3875c1af49ebbb
Dan Gleason <dannycgleason@gmail.com>
2022-08-12 20:20:58 -0400

    test

--- index.js
+++ index.js
@@ -14,6 +14,102 @@ const app = new App({
 app.command("/send", async ({ command, ack, say }) => {
     try {
         await ack();
+        // const result = await client.views.open({
+        //     trigger_id: shortcut.trigger_id,
+        //     view: {
+        //       type: "modal",
+        //       title: {
+        //         type: "plain_text",
+        //         text: "My App"
+        //       },
+        //       close: {
+        //         type: "plain_text",
+        //         text: "Close"
+        //       },
+
+        //       blocks: [
+        //         {
+        //             type: "input",
+        //             block_id: 'address_input',
+        //             element: {
+        //                 type: "plain_text_input",
+        //                 action_id: "plain_text_input-action"
+        //             },
+        //             label: {
+        //                 type: "plain_text",
+        //                 text: "Recipient street address",
+        //                 emoji: true
+        //             }
+        //         },
+        //         {
+        //             type: "input",
+        //             block_id: 'city_input',
+        //             element: {
+        //                 type: "plain_text_input",
+        //                 action_id: "plain_text_input-action"
+        //             },
+        //             label: {
+        //                 type: "plain_text",
+        //                 text: "Recipient city",
+        //                 emoji: true
+        //             }
+        //         },
+        //         {
+        //             type: "input",
+        //             block_id: 'state_input',
+        //             element: {
+        //                 type: "plain_text_input",
+        //                 action_id: "plain_text_input-action"
+        //             },
+        //             label: {
+        //                 type: "plain_text",
+        //                 text: "Recipient state/province",
+        //                 emoji: true
+        //             }
+        //         },
+        //         {
+        //             type: "input",
+        //             block_id: 'postal_input',
+        //             element: {
+        //                 type: "plain_text_input",
+        //                 action_id: "plain_text_input-action"
+        //             },
+        //             label: {
+        //                 type: "plain_text",
+        //                 text: "Recipient postal code",
+        //                 emoji: true
+        //             }
+        //         },
+        //         {
+        //             type: "input",
+        //             block_id: 'country_input',
+        //             element: {
+        //                 type: "plain_text_input",
+        //                 action_id: "plain_text_input-action"
+        //             },
+        //             label: {
+        //                 type: "plain_text",
+        //                 text: "Recipient country",
+        //                 emoji: true
+        //             }
+        //         },
+        //         {
+        //             type: "input",
+        //             block_id: 'message_input',
+        //             element: {
+        //                 type: "plain_text_input",
+        //                 multiline: true,
+        //                 action_id: "plain_text_input-action"
+        //             },
+        //             label: {
+        //                 type: "plain_text",
+        //                 text: "Your message",
+        //                 emoji: true
+        //             }
+        //         }
+        //     ]
+        //     }
+        //   });
         const result = await client.views.open({
             trigger_id: shortcut.trigger_id,
             view: {
@@ -28,88 +124,24 @@ app.command("/send", async ({ command, ack, say }) => {
               },
               blocks: [
                 {
-                    type: "input",
-                    block_id: 'address_input',
-                    element: {
-                        type: "plain_text_input",
-                        action_id: "plain_text_input-action"
-                    },
-                    label: {
-                        type: "plain_text",
-                        text: "Recipient street address",
-                        emoji: true
-                    }
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+                  }
                 },
                 {
-                    type: "input",
-                    block_id: 'city_input',
-                    element: {
-                        type: "plain_text_input",
-                        action_id: "plain_text_input-action"
-                    },
-                    label: {
-                        type: "plain_text",
-                        text: "Recipient city",
-                        emoji: true
+                  type: "context",
+                  elements: [
+                    {
+                      type: "mrkdwn",
+                      text: "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
                     }
-                },
-                {
-                    type: "input",
-                    block_id: 'state_input',
-                    element: {
-                        type: "plain_text_input",
-                        action_id: "plain_text_input-action"
-                    },
-                    label: {
-                        type: "plain_text",
-                        text: "Recipient state/province",
-                        emoji: true
-                    }
-                },
-                {
-                    type: "input",
-                    block_id: 'postal_input',
-                    element: {
-                        type: "plain_text_input",
-                        action_id: "plain_text_input-action"
-                    },
-                    label: {
-                        type: "plain_text",
-                        text: "Recipient postal code",
-                        emoji: true
-                    }
-                },
-                {
-                    type: "input",
-                    block_id: 'country_input',
-                    element: {
-                        type: "plain_text_input",
-                        action_id: "plain_text_input-action"
-                    },
-                    label: {
-                        type: "plain_text",
-                        text: "Recipient country",
-                        emoji: true
-                    }
-                },
-                {
-                    type: "input",
-                    block_id: 'message_input',
-                    element: {
-                        type: "plain_text_input",
-                        multiline: true,
-                        action_id: "plain_text_input-action"
-                    },
-                    label: {
-                        type: "plain_text",
-                        text: "Your message",
-                        emoji: true
-                    }
+                  ]
                 }
-            ]
+              ]
             }
           });
-      
           logger.info(result);
         } catch (error) {
             console.log("err")
